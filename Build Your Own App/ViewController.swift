501a5ff92bcf7eb0bb1b637298a2a3613610e7e8
Katherine Velazquez <kvela834@d127.org>
2022-12-19 09:19:59 -0600

    we finished:)

--- Build Your Own App/ViewController.swift
+++ Build Your Own App/ViewController.swift
@@ -25,34 +25,54 @@ class ViewController: UIViewController {
    
     @IBAction func colorRed(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "redDress"))
         englishLabel.text = "Red"
+        spanishLabel.text = "Rojo"
+        frenchLabel.text = "Rouge"
 
     }
     @IBAction func colorOrange(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "orange"))
         englishLabel.text = "Orange"
+        spanishLabel.text = "Naranja"
+        frenchLabel.text = "Orange"
     }
     @IBAction func colorYellow(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "yellow"))
         englishLabel.text = "Yellow"
+        spanishLabel.text = "Amarillo"
+        frenchLabel.text = "Jaune"
     }
     @IBAction func colorGreen(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "green"))
         englishLabel.text = "Green"
+        spanishLabel.text = "Verde"
+        frenchLabel.text = "Verte"
     }
     @IBAction func colorBlue(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "blueFlower"))
         englishLabel.text = "Blue"
+        spanishLabel.text = "Azul"
+        frenchLabel.text = "Bleue"
     }
     @IBAction func colorPurple(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "purple"))
         englishLabel.text = "Purple"
+        spanishLabel.text = "Morado"
+        frenchLabel.text = "Mauve"
     }
     @IBAction func colorWhite(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "white"))
         englishLabel.text = "White"
+        spanishLabel.text = "Blanco"
+        frenchLabel.text = "Blanche"
     }
     @IBAction func colorPink(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "pink"))
         englishLabel.text = "Pink"
+        spanishLabel.text = "Rosado"
+        frenchLabel.text = "Rose"
     }
     @IBAction func colorBlack(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "black"))
         englishLabel.text = "Black"
+        spanishLabel.text = "Negro"
+        frenchLabel.text = "Noire"
     }
     @IBAction func colorGrey(_ sender: UIButton) { print (photoChanger.image = UIImage (named: "greypt2"))
         englishLabel.text = "Grey"
+        spanishLabel.text = "Gris"
+        frenchLabel.text = "Grise"
     }
     
     
